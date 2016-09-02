562a5b340bff4b5587a06e18019375eeb940e9ba
Andre Fuechsel <andre.fuechsel@telekom.de>
2016-09-02 13:02:20 +0200

    works on Mac now
    Signed-off-by: Andre Fuechsel <andre.fuechsel@telekom.de>

---
+++ src/main/java/biz/wolter/minecraft/bukkit/NetworkInterfaceChecker.java
@@ -0,0 +1,160 @@
+package biz.wolter.minecraft.bukkit;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+import java.util.logging.Logger;
+
+/**
+ * {@link NetworkInterfaceChecker} implements a sophisticated method to find out a list of all
+ * {@link InetAddress}es of connected network interfaces.
+ */
+public class NetworkInterfaceChecker {
+
+    private final Logger logger;
+
+    public NetworkInterfaceChecker(Logger logger) {
+        this.logger = logger;
+    }
+
+    /**
+     * Check all network adapters for usable addresses.
+     *
+     * @return list of all {@link InetAddress}es of network interfaces, that are usable for UPnP
+     * @throws DiscoveryException for any error, that occurs
+     */
+    public List<InetAddress> getAddresses() {
+        return discoverBindAddresses(discoverNetworkInterfaces());
+    }
+
+    private List<NetworkInterface> discoverNetworkInterfaces() {
+        List<NetworkInterface> networkInterfaces = new ArrayList<NetworkInterface>();
+        try {
+            Enumeration<NetworkInterface> interfaceEnumeration = NetworkInterface.getNetworkInterfaces();
+            for (NetworkInterface iface : Collections.list(interfaceEnumeration)) {
+                if (isUsableNetworkInterface(iface)) {
+                    logger.fine("Discovered usable network interface: " + iface.getDisplayName());
+                    synchronized (networkInterfaces) {
+                        networkInterfaces.add(iface);
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            logger.warning("Could not not analyze local network interfaces due to " + ex.getMessage());
+        }
+
+        return networkInterfaces;
+    }
+
+    private List<InetAddress> discoverBindAddresses(List<NetworkInterface> networkInterfaces) {
+        List<InetAddress> bindAddresses = new ArrayList<InetAddress>();
+        try {
+            Iterator<NetworkInterface> it = networkInterfaces.iterator();
+            while (it.hasNext()) {
+                NetworkInterface networkInterface = it.next();
+
+                int usableAddresses = 0;
+                for (InetAddress inetAddress : getInetAddresses(networkInterface)) {
+                    if (inetAddress == null) {
+                        continue;
+                    }
+
+                    if (isUsableAddress(networkInterface, inetAddress)) {
+                        logger.fine("Discovered usable network interface address: " + inetAddress.getHostAddress());
+                        usableAddresses++;
+                        synchronized (bindAddresses) {
+                            bindAddresses.add(inetAddress);
+                        }
+                    }
+                }
+
+                if (usableAddresses == 0) {
+                    it.remove();
+                }
+            }
+        } catch (Exception ex) {
+            logger.warning("Could not not analyze local network interfaces due to " + ex.getMessage());
+        }
+
+        return bindAddresses;
+    }
+
+    private List<InetAddress> getInetAddresses(NetworkInterface networkInterface) {
+        return Collections.list(networkInterface.getInetAddresses());
+    }
+
+    /**
+     * Validation of every discovered network interface.
+     * <p>
+     * The given implementation ignores interfaces which are
+     * </p>
+     * <ul>
+     * <li>loopback (yes, we do not bind to lo0)</li>
+     * <li>down</li>
+     * <li>have no bound IP addresses</li>
+     * <li>named "vmnet*" (OS X VMWare does not properly stop interfaces when it quits)</li>
+     * <li>named "vnic*" (OS X Parallels interfaces should be ignored as well)</li>
+     * <li>named "*virtual*" (VirtualBox interfaces, for example</li>
+     * <li>named "ppp*"</li>
+     * </ul>
+     *
+     * @param iface The interface to validate.
+     * @return True if the given interface matches all validation criteria.
+     * @throws Exception If any validation test failed with an un-recoverable error.
+     */
+    private boolean isUsableNetworkInterface(NetworkInterface iface) throws Exception {
+        if (!iface.isUp()) {
+            return false;
+        }
+
+        if (getInetAddresses(iface).size() == 0) {
+            return false;
+        }
+
+        if (iface.getName().toLowerCase(Locale.ENGLISH).startsWith("vmnet") || (iface.getDisplayName() != null
+                && iface.getDisplayName().toLowerCase(Locale.ENGLISH).contains("vmnet"))) {
+            return false;
+        }
+
+        if (iface.getName().toLowerCase(Locale.ENGLISH).startsWith("vnic")) {
+            return false;
+        }
+
+        if (iface.getName().toLowerCase(Locale.ENGLISH).contains("virtual")) {
+            return false;
+        }
+
+        if (iface.getName().toLowerCase(Locale.ENGLISH).startsWith("ppp")) {
+            return false;
+        }
+
+        if (iface.getName().toLowerCase(Locale.ENGLISH).startsWith("utun")) {
+            return false;
+        }
+
+        if (iface.getName().toLowerCase(Locale.ENGLISH).startsWith("veth")) {
+            return false;
+        }
+
+        if (iface.getName().toLowerCase(Locale.ENGLISH).startsWith("docker")) {
+            return false;
+        }
+
+        if (iface.isLoopback()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean isUsableAddress(NetworkInterface networkInterface, InetAddress address) {
+        return (address instanceof Inet4Address && !address.isLoopbackAddress());
+    }
+
+}
--- src/main/java/biz/wolter/minecraft/bukkit/SmartHome.java
+++ src/main/java/biz/wolter/minecraft/bukkit/SmartHome.java
@@ -1,34 +1,24 @@
 package biz.wolter.minecraft.bukkit;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.logging.Logger;
 
 import javax.jmdns.JmDNS;
 import javax.jmdns.ServiceInfo;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriBuilder;
 
 import org.bukkit.Bukkit;
-import org.bukkit.World;
-import org.bukkit.World.Environment;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.media.sse.SseFeature;
 import org.glassfish.jersey.server.ResourceConfig;
-
-import com.google.gson.Gson;
 
 import biz.wolter.minecraft.bukkit.command.SendCommand;
 import biz.wolter.minecraft.bukkit.event.BlockChangeListener;
@@ -37,105 +27,117 @@ import biz.wolter.minecraft.bukkit.rest.RestResource;
 import biz.wolter.minecraft.bukkit.thing.ThingList;
 
 public class SmartHome extends JavaPlugin {
-	
-	public static final int DEFAULT_PORT = 10692;
-	
-	private final Logger logger = getLogger();
-	private final PluginManager pluginManager = Bukkit.getPluginManager();
-	
+
+    public static final int DEFAULT_PORT = 10692;
+
+    private final Logger logger = getLogger();
+    private final PluginManager pluginManager = Bukkit.getPluginManager();
+
     // REST Server
-	private HttpServer server;
-	// JmDNS
-	private JmDNS jmdns;
-	
+    private HttpServer server;
+    // JmDNS
+    private JmDNS jmdns;
+
     // Fired when plugin is first enabled
     @Override
     public void onEnable() {
-    	logger.info("Plugin enabled.");
-    	
-    	// Make default config from within resources available
-    	saveDefaultConfig();
-    	
-    	// Retrieve config values
-    	int port = getConfig().getInt("port", DEFAULT_PORT);
-    	
-    	// URI baseUri = UriBuilder.fromUri("http://localhost/").port(port).build();
-    	URI baseUri = UriBuilder.fromUri("http://0.0.0.0").port(port).build();
-    	ResourceConfig config = new ResourceConfig(RestResource.class, EventsBroadcasterResource.class, SseFeature.class);    	
-    	server = GrizzlyHttpServerFactory.createHttpServer(baseUri, config, true);
+        logger.info("Plugin enabled.");
 
-    	// Each plugin has its own "data folder"
-    	setThingList(ThingList.load(new File(getDataFolder(), "devices.json")));
-    	
-    	// Register event listener
-    	pluginManager.registerEvents(new BlockChangeListener(), this);
-    	
-    	// Register command (set an instance of command class as executor)
-    	this.getCommand("sendCommand").setExecutor(new SendCommand());   	
-    	
-		try {
+        // Make default config from within resources available
+        saveDefaultConfig();
 
-			// InetAddress addr = InetAddress.getLocalHost();
-			// Hack needed to get the real adapter and it's address
-			// See http://stackoverflow.com/questions/9481865/getting-the-ip-address-of-the-current-machine-using-java
-			InetAddress addr;
-			try(final DatagramSocket socket = new DatagramSocket()){
-				// IP and port are just placeholder to create a connection for the preferred adapter... 
-				socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
-				// ...which helps us to discover the "real" address
-				addr = socket.getLocalAddress();
-			}
-			
-	        // Getting IP address of localhost - getHostAddress returns IP Address in textual format
-	        logger.info("IP is " + addr.getHostAddress());
-			
+        // Retrieve config values
+        int port = getConfig().getInt("port", DEFAULT_PORT);
+
+        // URI baseUri = UriBuilder.fromUri("http://localhost/").port(port).build();
+        URI baseUri = UriBuilder.fromUri("http://0.0.0.0").port(port).build();
+        ResourceConfig config = new ResourceConfig(RestResource.class, EventsBroadcasterResource.class,
+                SseFeature.class);
+        server = GrizzlyHttpServerFactory.createHttpServer(baseUri, config, true);
+
+        // Each plugin has its own "data folder"
+        setThingList(ThingList.load(new File(getDataFolder(), "devices.json")));
+
+        // Register event listener
+        pluginManager.registerEvents(new BlockChangeListener(), this);
+
+        // Register command (set an instance of command class as executor)
+        this.getCommand("sendCommand").setExecutor(new SendCommand());
+
+        try {
+
+            // InetAddress addr = InetAddress.getLocalHost();
+            // Hack needed to get the real adapter and it's address
+            // See http://stackoverflow.com/questions/9481865/getting-the-ip-address-of-the-current-machine-using-java
+            // --> not working on Mac!
+            // InetAddress addr;
+            // try (final DatagramSocket socket = new DatagramSocket()) {
+            // // IP and port are just placeholder to create a connection for the preferred adapter...
+            // socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
+            // // ...which helps us to discover the "real" address
+            // addr = socket.getLocalAddress();
+            // }
+
+            NetworkInterfaceChecker networkChecker = new NetworkInterfaceChecker(logger);
+            List<InetAddress> addresses = networkChecker.getAddresses();
+
+            if (addresses.isEmpty()) {
+                logger.warning("No usable IP address found!");
+                return;
+            }
+
+            // Getting IP address of localhost - getHostAddress returns IP Address in textual format
+            logger.info("IP is " + addresses.get(0));
+
             // Create a JmDNS instance
-            jmdns = JmDNS.create(addr);
-            
+            jmdns = JmDNS.create(addresses.get(0));
+
             String serviceType = "_minecraft-server._tcp.local.";
             String serviceName = "smarthome";
             String serviceDescription = "Minecraft Smart Home";
             Hashtable<String, String> serviceProperties = new Hashtable<String, String>();
             serviceProperties.put("uri", "/rest");
-            
+
             // Register a service
             // ServiceInfo serviceInfo = ServiceInfo.create(serviceType, serviceName, port, serviceDescription);
-            ServiceInfo serviceInfo = ServiceInfo.create(serviceType, serviceName, port, 0,0, serviceProperties);
+            ServiceInfo serviceInfo = ServiceInfo.create(serviceType, serviceName, port, 0, 0, serviceProperties);
 
             jmdns.registerService(serviceInfo);
-		} catch (IOException e) {
-			logger.warning(e.getMessage());
-		}		
-		
+        } catch (IOException e) {
+            logger.warning(e.getMessage());
+        }
+
     }
+
     // Fired when plugin is disabled
     @Override
     public void onDisable() {
-    	logger.info("Plugin disabled.");
-    	
-    	// Store changes
-    	if (!thingList.isSavedImmediately()) {
-    		getThingList().save();
-    	}
-    	
-    	// Unregister all services
+        logger.info("Plugin disabled.");
+
+        // Store changes
+        if (!thingList.isSavedImmediately()) {
+            getThingList().save();
+        }
+
+        // Unregister all services
         jmdns.unregisterAllServices();
-    	server.shutdownNow();
+        server.shutdownNow();
     }
-    
-	private ThingList thingList;    
-    
-	/**
-	 * @return the thingList
-	 */
-	public ThingList getThingList() {
-		return thingList;
-	}
-	/**
-	 * @param thingList the thingList to set
-	 */
-	private void setThingList(ThingList thingList) {
-		this.thingList = thingList;
-	}
-    
+
+    private ThingList thingList;
+
+    /**
+     * @return the thingList
+     */
+    public ThingList getThingList() {
+        return thingList;
+    }
+
+    /**
+     * @param thingList the thingList to set
+     */
+    private void setThingList(ThingList thingList) {
+        this.thingList = thingList;
+    }
+
 }
